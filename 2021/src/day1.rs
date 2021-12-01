5e48fcd2422c8d01303a63fbdf4600db86b1fead
Clayton Ferris <cferris@gmail.com>
2021-11-30 22:47:18 -0700

    2021 day 1

--- 2021/src/day1.rs
+++ 2021/src/day1.rs
@@ -7,16 +7,35 @@ fn parse_input_day1(input: &str) -> Result<Vec<i32>, ParseIntError> {
 }
 
 #[aoc(day1, part1)]
-fn part1(i: &[i32]) -> i32 {
+fn part1(i: &[i32]) -> usize {
   i.iter()
-    .copied()
-    .find_map(
-      |j| match i.iter().copied().find(|k| (j + k) as i32 == 2020) {
-        Some(k) => Some((j * k) as i32),
-        None => None,
-      },
-    )
-    .unwrap_or(0)
+    .enumerate()
+    .filter(|(idx, depth)| match i.get(idx + 1) {
+      Some(next_depth) => *depth < next_depth,
+      None => false,
+    })
+    .count()
+}
+
+#[aoc(day1, part2)]
+fn part2(i: &[i32]) -> usize {
+  // create 3 element sums
+  let sums: Vec<i32> = i
+    .iter()
+    .enumerate()
+    .filter_map(|(idx, depth_a)| match (i.get(idx + 1), i.get(idx + 2)) {
+      (Some(depth_b), Some(depth_c)) => Some(depth_a + depth_b + depth_c),
+      _ => None,
+    })
+    .collect();
+  sums
+    .iter()
+    .enumerate()
+    .filter(|(idx, depth)| match sums.get(idx + 1) {
+      Some(next_depth) => *depth < next_depth,
+      None => false,
+    })
+    .count()
 }
 
 #[cfg(test)]
@@ -25,6 +44,17 @@ mod tests {
 
   #[test]
   fn part1_example() {
-    assert_eq!(part1(&[1721, 979, 366, 299, 675, 1456]), 514579)
+    assert_eq!(
+      part1(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263]),
+      7
+    )
+  }
+
+  #[test]
+  fn part2_example() {
+    assert_eq!(
+      part2(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263]),
+      5
+    )
   }
 }
--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -1,3 +1,4 @@
 use aoc_runner_derive::aoc_main;
+extern crate advent_of_code_2021;
 
 aoc_main! { lib = advent_of_code_2021 }
